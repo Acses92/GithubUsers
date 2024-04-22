8b8c25cfe06983f21de4c1dff658ed9233282a9f
Acses92 <40656486+Acses92@users.noreply.github.com>
2024-04-22 09:59:24 +0500

    Add init navigation method.

--- app/src/main/java/ru/kravchenkoanatoly/githubusers/LaunchActivity.kt
+++ app/src/main/java/ru/kravchenkoanatoly/githubusers/LaunchActivity.kt
@@ -2,6 +2,7 @@ package ru.kravchenkoanatoly.githubusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import ru.kravchenkoanatoly.githubusers.databinding.LaunchActivityBinding
 
@@ -13,5 +14,11 @@ class LaunchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = LaunchActivityBinding.inflate(layoutInflater)
         setContentView(R.layout.launch_activity)
+        navigationSetup()
+    }
+
+    private fun navigationSetup() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
