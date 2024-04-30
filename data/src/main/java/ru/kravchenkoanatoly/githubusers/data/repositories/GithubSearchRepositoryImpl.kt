8681d1df4ac8253413bc4a9e280ba836b6997aed
Acses92 <40656486+Acses92@users.noreply.github.com>
2024-05-01 00:16:31 +0500

    Add ErrorParser, fix codestyle.

--- data/src/main/java/ru/kravchenkoanatoly/githubusers/data/repositories/GithubSearchRepositoryImpl.kt
+++ data/src/main/java/ru/kravchenkoanatoly/githubusers/data/repositories/GithubSearchRepositoryImpl.kt
@@ -3,9 +3,7 @@ package ru.kravchenkoanatoly.githubusers.data.repositories
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.forEach
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
+import ru.kravchenkoanatoly.githubusers.data.base.ErrorParser
 import ru.kravchenkoanatoly.githubusers.data.mappers.toEntity
 import ru.kravchenkoanatoly.githubusers.data.sources.database.AppDatabase
 import ru.kravchenkoanatoly.githubusers.data.sources.remote.GithubApi
@@ -15,29 +13,32 @@ import javax.inject.Inject
 
 class GithubSearchRepositoryImpl @Inject constructor(
     private val githubApi: GithubApi,
-    private val appDatabase: AppDatabase
-): GithubSearchRepository {
-    override fun searchUser(userRequest: String, pageSize: Int, page: Int): Flow<Unit> = flow<Unit> {
-        val items = githubApi.searchUsers(userRequest, pageSize,page).items
-            if(items.isNotEmpty()){
-                items.onEach { //appDatabase.githubSearchUserDao().deleteAll()
-                }
-                    .map { dto ->
-                        dto?.toEntity() }
-                    .forEach {
-                        if (it != null) {
-                            appDatabase.githubSearchUserDao().insert(it)
-                        }
+    private val appDatabase: AppDatabase,
+    private val errorParser: ErrorParser
+) : GithubSearchRepository {
+    override fun searchUser(userRequest: String, pageSize: Int, page: Int): Flow<Unit> =
+            flow<Unit> {
+                val items = githubApi.searchUsers(userRequest, pageSize, page).items
+                if (items.isNotEmpty()) {
+                    items.onEach { //appDatabase.githubSearchUserDao().deleteAll()
                     }
-            } else {
-                appDatabase.githubSearchUserDao().deleteAll()
-            }
+                        .map { dto ->
+                            dto?.toEntity()
+                        }
+                        .forEach {
+                            if (it != null) {
+                                appDatabase.githubSearchUserDao().insert(it)
+                            }
+                        }
+                } else {
+                    appDatabase.githubSearchUserDao().deleteAll()
+                }
 
-    }.catch {Timber.tag(GITHUB_SEARCH_REPOSITORY_TAG).d(it)  }
+            }.catch { errorParser.invoke(it) }
 
     override fun getMaxUsersFromRequest(userRequest: String): Flow<Int> = flow<Int> {
-        emit(githubApi.searchUsers(userRequest, 1,1).totalCount)
-    }.catch { Timber.tag(GITHUB_SEARCH_REPOSITORY_TAG).d(it) }
+        emit(githubApi.searchUsers(userRequest, 1, 1).totalCount)
+    }.catch { errorParser.invoke(it) }
 
 
     companion object {
